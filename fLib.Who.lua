ae5bc91bac8812414bcb3d1519c8e4b4c00ea23c
jessica.huang <jessica.huang@7aec42f0-9e3d-11dd-ada7-d1ebab521e76>
2009-08-12 17:51:03 +0000

    git-svn-id: https://tamaddons.googlecode.com/svn/trunk/fLib@190 7aec42f0-9e3d-11dd-ada7-d1ebab521e76

---
+++ fLib.Who.lua
@@ -0,0 +1,15 @@
+fLib.Who = {}
+local wholist = {} --list of people you've request info about
+--key = player name
+--value = player info: rank, level, class, online, status
+
+
+--returns info about name if we have it
+--otherwise returns nil
+function fLib.Who.Info(name)
+	local info = wholist[name]
+	if not info then
+	end
+	return info
+end
+
